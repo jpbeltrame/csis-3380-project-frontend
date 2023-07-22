23091d01c4d596bee90e804f72d251c77ad9512a
Mariana Moura <marii.teoc@gmail.com>
2023-07-21 21:17:12 -0700

    front end design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
+import "./App.css";
+import { Layout, Menu } from "antd";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import { Content, Footer } from "antd/es/layout/layout";
 
 function App() {
+
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <Layout hasSider="true">
+          <Header />
+          <Layout>
+            <Content>
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
+            </Content>
+            
+          </Layout>
+          <Footer> foooo</Footer>
+        </Layout>
       </div>
     </BrowserRouter>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,26 @@
 import './Header.css';
+import React, { useState } from 'react';
+import { Avatar ,Button,Menu, Layout} from 'antd';
+import { Typography } from 'antd';
+import { UserOutlined} from '@ant-design/icons';
+
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+
+const items = [
+  getItem('User Name', 'sub1', <Avatar />, [
+    getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
+    getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
+  ])];
+
 
 function Header() {
   const routes = [
@@ -13,14 +35,53 @@ function Header() {
   ];
 
   const menuItens = routes.map((r) => <li><a href={r.path}>{r.name}</a></li>);
+  const { Title } = Typography;
+  const url = 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg';
+ 
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+  
   return (
     <div className="header">
-      <span>BookTrakr</span>
-      <ul>
-        {menuItens}
-      </ul>
+      
+      <Title style={{ background: "#27024B", color:"white", padding:'4%',fontSize:'40px'}}>BookTrackr
+      
+      <div
+      style={{
+        width: 256,
+        float:'right'
+      }}
+    >
+
+      <Menu
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+        theme="dark"
+        inlineCollapsed={collapsed}
+        items={items}
+      />
     </div>
+    <Avatar onClick={toggleCollapsed} src={<img src={url} />} size={60} style={{float:'right', margin:'5px',backgroundColor: '#87d068'}} icon={<UserOutlined />}>
+      <Layout.Sider style={{ background: "#27024B" }}>
+      {collapsed ?  <img src='none' />: <Avatar />}
+            </Layout.Sider>
+            </Avatar>
+      </Title>
+      
+      {/* <ul>
+        {menuItens}
+      </ul> */}
+ 
+    <div >
+      
+    </div>
+    </div>
+    
   );
+
 }
 
 export default Header;
